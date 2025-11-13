76e24c58939d55d5c5bde16c3e77f8729dc65fee
oKukHtina <olakukhtina95@gmail.com>
2025-11-13 18:03:07 +0200

    optimized specification

--- src/main/java/com/example/repository/specification/impl/BookSpecificationBuilder.java
+++ src/main/java/com/example/repository/specification/impl/BookSpecificationBuilder.java
@@ -8,6 +8,8 @@ import com.example.entity.Book;
 import com.example.exception.InvalidProviderException;
 import com.example.repository.specification.SpecificationBuilder;
 import com.example.repository.specification.SpecificationProviderManager;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.ArrayUtils;
 import org.springframework.data.jpa.domain.Specification;
@@ -21,30 +23,28 @@ public class BookSpecificationBuilder implements SpecificationBuilder<Book> {
 
     @Override
     public Specification<Book> build(BookSearchParametersDto params) {
-        Specification<Book> spec = Specification.where(null);
-
-        boolean hasValidParam = false;
+        List<Specification<Book>> specs = new ArrayList<>();
 
         if (ArrayUtils.isNotEmpty(params.authors())) {
-            hasValidParam = true;
-            spec = spec.and(specificationProviderManager
+            specs.add(specificationProviderManager
                     .getSpecificationProvider(AUTHOR_KEY)
                     .getSpecification(params.authors()));
         }
 
         if (ArrayUtils.isNotEmpty(params.titles())) {
-            hasValidParam = true;
-            spec = spec.and(specificationProviderManager
+            specs.add(specificationProviderManager
                     .getSpecificationProvider(TITLE_KEY)
                     .getSpecification(params.titles()));
         }
 
-        if (!hasValidParam) {
+        if (specs.isEmpty()) {
             throw new InvalidProviderException(
                     "Invalid search parameter. Allowed: 'authors', 'titles'."
             );
         }
 
-        return spec;
+        return specs.stream()
+                .reduce(Specification::and)
+                .orElseThrow(() -> new InvalidProviderException("No valid specification found"));
     }
 }
