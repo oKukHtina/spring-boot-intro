9644cdf66cc6a54a2a8010c57d36b948cc63c92e
oKukHtina <olakukhtina95@gmail.com>
2025-11-17 23:17:05 +0200

    Added Pagination, Sorting, and Swagger to the existing controllers.

--- src/main/java/com/example/controller/BookController.java
+++ src/main/java/com/example/controller/BookController.java
@@ -4,9 +4,13 @@ import com.example.dto.BookDto;
 import com.example.dto.BookSearchParametersDto;
 import com.example.dto.CreateBookRequestDto;
 import com.example.service.BookService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Book management", description = "Endpoints for managing books")
 @RestController
 @RequestMapping("/books")
 @RequiredArgsConstructor
@@ -25,11 +30,13 @@ public class BookController {
     private final BookService bookService;
 
     @GetMapping
-    public List<BookDto> getAll() {
-        return bookService.findAll();
+    @Operation(summary = "Get all books", description = "Get page of all available books")
+    public Page<BookDto> getAll(Pageable pageable) {
+        return bookService.findAll(pageable);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Book by Id", description = "Return book by Id")
     public BookDto getBookById(
             @PathVariable Long id
     ) {
@@ -37,6 +44,7 @@ public class BookController {
     }
 
     @GetMapping("/search")
+    @Operation(summary = "Search book", description = "Get a list of books by specification")
     public List<BookDto> searchBooks(
             BookSearchParametersDto searchParameters
     ) {
@@ -45,6 +53,7 @@ public class BookController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Create a book", description = "Create a new book")
     public BookDto createBook(
             @RequestBody @Valid CreateBookRequestDto bookDto
     ) {
@@ -52,6 +61,7 @@ public class BookController {
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Update book", description = "Update book by Id")
     public BookDto updateBook(
             @PathVariable Long id,
             @RequestBody @Valid CreateBookRequestDto bookDto
@@ -61,6 +71,7 @@ public class BookController {
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete book", description = "Delete a book by Id")
     public void deleteBookById(
             @PathVariable Long id
     ) {
