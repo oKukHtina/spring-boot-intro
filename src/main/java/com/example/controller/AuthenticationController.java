25c7f6983092e848f9ec94e51182a40aeb1e6a2b
oKukHtina <olakukhtina95@gmail.com>
2025-12-04 23:08:33 +0200

    added swagger annotations

--- src/main/java/com/example/controller/AuthenticationController.java
+++ src/main/java/com/example/controller/AuthenticationController.java
@@ -4,20 +4,30 @@ import com.example.dto.UserRegistrationRequestDto;
 import com.example.dto.UserResponseDto;
 import com.example.exception.RegistrationException;
 import com.example.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "User management", description = "Endpoints for managing users")
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
     private final UserService userService;
 
+    @Operation(
+            summary = "Register a new user",
+            description = "Allows to register a new user in the system"
+    )
     @PostMapping("/registration")
+    @ResponseStatus(HttpStatus.CREATED)
     public UserResponseDto register(
             @RequestBody @Valid UserRegistrationRequestDto request
     ) throws RegistrationException {
