f8edce4a8915d75e31f853433a7fd818c88f901d
oKukHtina <olakukhtina95@gmail.com>
2026-02-03 23:55:59 +0200

    Added User entity with roles

---
+++ src/main/java/com/example/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.example.repository;
+
+import com.example.entity.Role;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByRoleName(Role.RoleName roleName);
+}
